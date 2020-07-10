e1019244eada2fa9074e1aa8aa7f781183e88ce1
MayankBatra005 <er.mayank005@gmail.com>
2020-07-10 10:48:23 -0700

    Intial Project commit

---
+++ Assignment/Assignment 1/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/covidtracker/Activities/HomeScreen.java
@@ -0,0 +1,19 @@
+package edu.asu.msse.mbatra3.covidtracker.Activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import edu.asu.msse.mbatra3.covidtracker.R;
+import android.view.View;
+
+public class HomeScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+    }
+
+    public void updateChatWindow(View v)
+    {
+
+    }
+}
