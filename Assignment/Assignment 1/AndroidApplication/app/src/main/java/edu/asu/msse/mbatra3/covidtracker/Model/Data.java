d28be8cf6e3c50c76fa2d487012fe1e75058d799
MayankBatra005 <er.mayank005@gmail.com>
2020-07-11 18:16:00 -0700

    Database initial code

--- Assignment/Assignment 1/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/covidtracker/Model/Data.java
+++ Assignment/Assignment 1/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/covidtracker/Model/Data.java
@@ -1,4 +1,37 @@
 package edu.asu.msse.mbatra3.covidtracker.Model;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 public class Data {
+
+    public boolean initDB(String dbName, Context context)
+    {
+        boolean isDbInit=false;
+        try{
+            SQLiteDatabase db=SQLiteDatabase.openOrCreateDatabase(dbName,null);
+            String sql="CREATE TABLE IF NOT EXISTS "+dbName+"( XCOORDINATE VARCHAR," +
+                    "YCOORDINATE VARCHAR, TIMESTAMP VARCHAR)";
+            db.execSQL(sql);
+            isDbInit= true;
+
+        }
+        catch (Exception E)
+        {
+            E.printStackTrace();
+        }
+        return isDbInit;
+    }
+
+    public boolean insertData(String x,String y,String timeStamp)
+    {
+
+        try {
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
 }
