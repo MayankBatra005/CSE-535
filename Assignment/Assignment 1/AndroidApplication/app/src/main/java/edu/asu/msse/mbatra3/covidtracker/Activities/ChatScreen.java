b54ae809f13325a1de88b0f1e3517b04ff5674d2
MayankBatra005 <er.mayank005@gmail.com>
2020-07-20 22:21:01 -0700

    Chat code in progress....

--- Assignment/Assignment 1/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/covidtracker/Activities/ChatScreen.java
+++ Assignment/Assignment 1/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/covidtracker/Activities/ChatScreen.java
@@ -2,25 +2,39 @@ package edu.asu.msse.mbatra3.covidtracker.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.concurrent.ExecutionException;
+
 import androidx.appcompat.app.AppCompatActivity;
 import edu.asu.msse.mbatra3.covidtracker.R;
+import edu.asu.msse.mbatra3.covidtracker.utilities.ChatHelper;
 
 public class ChatScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_screen);
+
     }
 
-    public void updateChatWindow(View v) throws InterruptedException {
+    public void updateChatWindow(View v) {
         EditText chatMessage=findViewById(R.id.chatMessage);
         TextView chatWindow=findViewById(R.id.chatWindow);
         chatWindow.setText("Chat updated "+chatMessage.getText());
         chatMessage.setText(" ");
+        ChatHelper task= new ChatHelper();
+        String result=null;
+        try {
+            result =
+//            task.execute("https://www.google.com/").get();
+                    task.execute("http://10.218.107.121/").get();
+        }catch (Exception E){
+
+        }    Log.i("Result from ASU server",result);
 
         Intent location=new Intent(this,LocationScreen.class);
         startActivity(location);
