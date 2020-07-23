f2ffef0c44fcdc198c674a2b3ffc0d7c4e4b6cb5
Mayank Batra <37073720+MayankBatra005@users.noreply.github.com>
2020-07-23 07:22:28 -0700

    Service initialisation bug squashed.

--- Assignment/Assignment 1/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/covidtracker/Activities/HomeScreen.java
+++ Assignment/Assignment 1/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/covidtracker/Activities/HomeScreen.java
@@ -7,12 +7,24 @@ import edu.asu.msse.mbatra3.covidtracker.R;
 import edu.asu.msse.mbatra3.covidtracker.utilities.UnboundDataPostingService;
 
 import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+
+        Button bt1 = (Button) findViewById(R.id.locationConsent);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeScreen.this, UnboundDataPostingService.class);
+                startService(intent);
+            }
+        });
+
     }
     public void moveToChatScreen(View v){
     Intent chatScreen= new Intent(this,ChatScreen.class);
@@ -21,8 +33,11 @@ public class HomeScreen extends AppCompatActivity {
 
     public void activateService(View view)throws  Exception{
         // Starting the service
-            Intent intent = new Intent(this, UnboundDataPostingService.class);
+            Intent intent = new Intent(HomeScreen.this, UnboundDataPostingService.class);
             startService(intent);
+
+//        Intent int1 = new Intent(MainActivity.this, MyServiceExTuesday.class);
+//        startService(int1);
          //   Thread.sleep(5000);
         // Stopping the service after 5 seconds
         // stopService(intent);
--- Assignment/Assignment 1/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/covidtracker/utilities/UnboundDataPostingService.java
+++ Assignment/Assignment 1/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/covidtracker/utilities/UnboundDataPostingService.java
@@ -9,26 +9,29 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 
 public class UnboundDataPostingService extends Service {
+    public UnboundDataPostingService(){}
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i("On Start", "Service Started");
-        return super.onStartCommand(intent, flags, startId);
+//        return super.onStartCommand(intent, flags, startId);
+        Toast.makeText(this, "Hey there ",Toast.LENGTH_SHORT).show();
+        return Service.START_NOT_STICKY;
     }
-    @Override
-    public void onCreate() {
-        Log.i("On Create", "Service Created");
-    }
-
-    @Override
-    public void onDestroy() {
-        Log.i("On Destroy", "Service Destroyed");
-    }
+//    @Override
+//    public void onCreate() {
+//        Log.i("On Create", "Service Created");
+//    }
+//
+//    @Override
+//    public void onDestroy() {
+//        Log.i("On Destroy", "Service Destroyed");
+//    }
 
 
     /* Driver code : To be written in Driving class
