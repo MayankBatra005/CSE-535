59d8083b50bd3335a623ecd39abcc4b589adf995
MayankBatra005 <er.mayank005@gmail.com>
2020-07-22 21:50:53 -0700

    Service backup

--- Assignment/Assignment 1/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/covidtracker/Activities/HomeScreen.java
+++ Assignment/Assignment 1/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/covidtracker/Activities/HomeScreen.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import edu.asu.msse.mbatra3.covidtracker.R;
+import edu.asu.msse.mbatra3.covidtracker.utilities.UnboundDataPostingService;
+
 import android.view.View;
 
 public class HomeScreen extends AppCompatActivity {
@@ -15,6 +17,16 @@ public class HomeScreen extends AppCompatActivity {
     public void moveToChatScreen(View v){
     Intent chatScreen= new Intent(this,ChatScreen.class);
     startActivity(chatScreen);
+    }
+
+    public void activateService(View view)throws  Exception{
+        // Starting the service
+            Intent intent = new Intent(this, UnboundDataPostingService.class);
+            startService(intent);
+         //   Thread.sleep(5000);
+        // Stopping the service after 5 seconds
+        // stopService(intent);
+
     }
 
 }
--- Assignment/Assignment 1/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/covidtracker/utilities/UnboundDataPostingService.java
+++ Assignment/Assignment 1/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/covidtracker/utilities/UnboundDataPostingService.java
@@ -3,6 +3,7 @@ package edu.asu.msse.mbatra3.covidtracker.utilities;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -16,17 +17,17 @@ public class UnboundDataPostingService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        Log.i("On Start", "Service Started");
         return super.onStartCommand(intent, flags, startId);
     }
     @Override
     public void onCreate() {
-        Toast.makeText(this, "Service Created", Toast.LENGTH_LONG).show();
+        Log.i("On Create", "Service Created");
     }
 
     @Override
     public void onDestroy() {
-        Toast.makeText(this, "Service Stopped", Toast.LENGTH_LONG).show();
+        Log.i("On Destroy", "Service Destroyed");
     }
 
 
