92ee60c60af2d8a22109e3a1c5f6288859f3fe9e
MayankBatra005 <er.mayank005@gmail.com>
2020-08-07 10:15:59 -0700

    build version 1.3 EC achived :)

--- Project/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/mobileoffloading/EstimatedCalulationsView.java
+++ Project/AndroidApplication/app/src/main/java/edu/asu/msse/mbatra3/mobileoffloading/EstimatedCalulationsView.java
@@ -10,9 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class EstimatedCalulationsView extends AppCompatActivity {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.offload_calculations_screen);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.estimated_calculations_screen);
         Intent intent = getIntent();
         ((TextView) findViewById(R.id.estimation1)).setText(intent.
                 getStringExtra("estimation1"));
